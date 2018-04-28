117a8dbbe850d2640110f0bca3b0034a60123945
Mattia Ninivaggi <ninivmat@students.zhaw.ch>
2018-04-29 00:59:18 +0200

    initial js stuff

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+$(document).ready(function () {
 
+    var fakemainboard = [
+        {
+            "name" : "SomeBoard", 
+            "id": "1", 
+            "price" : 1
+        },
+        {
+            "name" : "SomeBoard2", 
+            "id": "2",
+            "price" : 2
+        },
+        {
+            "name" : "SomeBoard3", 
+            "id": "3",
+            "price" : 3
+        }
+    ];
+
+    // data is an array
+    function attachDataToOption(data, id){
+        data.forEach(opt => {
+            var $opt = $("<option>",
+            {
+                value: opt.id,
+            }).val(opt.name);
+            
+            $opt.appendTo($(id));
+        });
+
+    };
+
+    attachDataToOption(fakemainboard, "mainboardList");
+
+});
