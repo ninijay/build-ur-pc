b79315668994f34a9b942b1f7a0e4d2e73b14dde
Mattia Ninivaggi <ninivmat@students.zhaw.ch>
2018-04-29 02:53:22 +0200

    automatic fields generation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var fakemainboard = [
+    window.fakemainboard = [
         {
             "name" : "SomeBoard", 
             "id": "1", 
@@ -18,19 +18,127 @@ $(document).ready(function () {
         }
     ];
 
+    window.fakeCPU = [
+        {
+            "name" : "asd", 
+            "id": "1", 
+            "price" : 1
+        },
+        {
+            "name" : "sdfs", 
+            "id": "2",
+            "price" : 2
+        },
+        {
+            "name" : "sdfsd", 
+            "id": "3",
+            "price" : 3
+        }
+    ];
+    function calcTotal()
+    {
+        var i = 0;
+        $("[id$='Price']").each(function(){
+            console.log($(this).data("price"));
+
+            i+= parseInt($(this).data("price")) || 0;
+        });
+        return i;
+    }
+
     // data is an array
     function attachDataToOption(data, id){
+        var listId = "#"+id+"List";
+        var PriceId = "#"+id+"Price";
         data.forEach(opt => {
             var $opt = $("<option>",
             {
                 value: opt.id,
-            }).val(opt.name);
-            
-            $opt.appendTo($(id));
+            }).text(opt.name);
+            $opt.data("price", opt.price);
+            $opt.appendTo($(listId));
         });
-
+     
+        
+        $(listId).on("change", function(){
+            var price = $("option:selected", this).data("price");
+            $(PriceId).html("Price: "+ price);
+            $(PriceId).data("price", price);
+            $("#result").html("Total Price: " + calcTotal());
+        });
     };
 
-    attachDataToOption(fakemainboard, "mainboardList");
+    function render(){
+        var cats = [
+            "mainboard",
+            "CPU", 
+            "GPU",
+            "RAM",
+            "cooling",
+            "system"
+        ];
+    
+        var cols = 1;
+        var $row = $("<div>", {
+            "class" : "row"
+        });
+    
+        var $main = $("#main");
+        
+        var check = 3;
+        if(cats.length < 3)
+        {
+            check = cats.length;
+        }
+
+        cats.forEach(element => {
+                var $col = $("<div>", {
+                    class : "col-md-4",
+                    id : element + "Main"
+                });
+                cols++;
+                $("<h2>").text(element).appendTo($col);
+                var $p = $("<p>");
+                var $grp = $("div", {
+                    "class" : "form-group"
+                });
+                var $lbl = $("<label>", {
+                    "for" : element+"List"
+                }).text("Select " + element);
+                var $select = $("<select>", {
+                    class: "form-control",
+                    id : element+"List"
+                });
+                var $price = $("<p>",
+                {
+                    id : element + "Price"
+                }).text("Price:");
+
+                $grp.appendTo($p);
+                $lbl.appendTo($p);
+                $select.appendTo($p);
+                $p.appendTo($col);
+                $price.appendTo($col);
+                $col.appendTo($row);
+                $row.appendTo($main);
+                console.log($row);  
+        });
+
+        $row = $("<div>", {
+            class:"row"
+        });
+
+        var $total = $("<h2>",{
+            id : "result"
+        }).text("Total Price:");
+
+        $total.appendTo($row);
+        $row.appendTo($main);
+
+    }
+    render();
+    attachDataToOption(window.fakemainboard, "mainboard");
+    attachDataToOption(window.fakeCPU, "CPU");
 
 });
+
